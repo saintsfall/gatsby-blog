44b57f2141602295fbdc4a1a74c3c026e14ce8ca
Caio Santos <caio.f.santos@icloud.com>
2019-09-20 19:02:00 -0300

    Used the node api on gatsby to generate the slugs, updated the PostItem to use it.

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,6 +19,9 @@ const IndexPage = () => {
               title
             }
             timeToRead
+            fields {
+              slug
+            }
           }
         }
       }
@@ -35,10 +38,11 @@ const IndexPage = () => {
           node: {
             frontmatter: { background, category, date, description, title },
             timeToRead,
+            fields: { slug },
           },
         }) => (
           <PostItem
-            slug="/slug/"
+            slug={slug}
             background={background}
             category={category}
             date={date}
